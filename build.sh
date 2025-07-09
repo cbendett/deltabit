da5bc7dc566fcb9c8697cb7c6c7d5eb2a592298a
conor bendett <98918758+cbendett@users.noreply.github.com>
2025-07-09 11:00:51 -0500

    Added files for building conda environment

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd $SRC_DIR
+mkdir -p $PREFIX/bin
+cp -r bin/* $PREFIX/bin/
+
---
+++ post-link.sh
@@ -0,0 +1,3 @@
+# post-link.sh
+
+Rscript -e 'install.packages("geneviewer", repos="https://cloud.r-project.org")'
